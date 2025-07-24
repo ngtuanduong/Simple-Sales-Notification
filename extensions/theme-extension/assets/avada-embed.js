bce5c7a8ba3090794d40dc64e2144011646663e6
Nguyen Tuan  Duong <ntduongvbhp@gmail.com>
2025-07-25 03:36:01 +0700

    ok

--- extensions/theme-extension/assets/avada-embed.js
+++ extensions/theme-extension/assets/avada-embed.js
@@ -1,5 +1,5 @@
 (function() {
-  const BASE_URL = 'https://frankfurt-sea-ski-assessment.trycloudflare.com/scripttag';
+  const BASE_URL = 'https://antibodies-speaks-cake-jersey.trycloudflare.com/scripttag';
 
   const scriptElement = document.createElement('script');
   scriptElement.type = 'text/javascript';
