b2506c0f04ff894159f305aa7d7abf634ba5e335
Nguyen Tuan  Duong <ntduongvbhp@gmail.com>
2025-08-01 13:44:46 +0700

    update dashboard fe

--- packages/assets/src/pages/Home/Home.js
+++ packages/assets/src/pages/Home/Home.js
@@ -9,7 +9,6 @@ import {MaxModalContext} from '@assets/contexts/maxModalContext';
  * @constructor
  */
 export default function Home() {
-  const [enabled, setEnabled] = useState(false);
   const {openFullscreen} = useContext(MaxModalContext);
 
   return (
@@ -18,22 +17,10 @@ export default function Home() {
         <Layout.Section>
           <BlockStack gap="400">
             <Card>
-              <InlineStack blockAlign="center">
-                <Text as="span">Our app is {enabled ? 'enabled' : 'disabled'} on your store</Text>
-                <div style={{flex: 1}} />
-                <Button
-                  variant={enabled ? 'secondary' : 'primary'}
-                  onClick={() => setEnabled(prev => !prev)}
-                >
-                  {enabled ? 'Disable' : 'Enable'}
-                </Button>
-              </InlineStack>
-            </Card>
-            <Card>
               <InlineStack gap="200" blockAlign="center">
                 <Text as="span">Fullscreen</Text>
-                <Button onClick={() => openFullscreen('/Notifications')}>Samples</Button>
                 <Button onClick={() => openFullscreen('/settings')}>Settings</Button>
+                <Button onClick={() => openFullscreen('/notifications')}>Notifications</Button>
               </InlineStack>
             </Card>
           </BlockStack>
